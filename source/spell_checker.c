5ec6d58ce13c047d599eae0eac00bb4cdfa921cc
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-07 10:44:21 -0300

    comments added

--- source/spell_checker.c
+++ source/spell_checker.c
@@ -5,6 +5,7 @@
 
 #define NWORDS_TEXT 1000
 #define DICTIONARY_LEN 320139
+#define MAX_INPUT 281
 
 char **getDictionary(char[]);
 
@@ -28,26 +29,22 @@ int main(int argc, char argv[]){
     printf("Ou digite 'T' e ENTER para digitar o texto no terminal.\n");
     printf("> ");
 
-    char input[280];
-    fgets(input, 280, stdin);
+    char input[MAX_INPUT];
+    fgets(input, MAX_INPUT, stdin);
 
     if (strlen(input) == 2 && input[0] == 'T') {
         printf("> ");
-        fgets(input, 280, stdin);
+        fgets(input, MAX_INPUT, stdin);
 
         userText = getWordsFromLine(input, &numberOfWords);
-
-        printf("%d\n", numberOfWords);
-        printf("%s\n", userText[numberOfWords - 1]);
-        // Fazer nossas operações...
     } else {
         input[strlen(input) - 1] = '\0';
         userText = getTextFromFile(input, &numberOfWords);
-
-        printf("%d\n", numberOfWords);
-        printf("%s\n", userText[numberOfWords - 1]);
-        // Fazer nossas operações...
     }
+
+    printf("%d\n", numberOfWords);
+    printf("%s\n", userText[numberOfWords - 1]);
+    // Fazer nossas operações...
 
     //printf("O texto foi: \n%s", text);
 
@@ -151,34 +148,46 @@ char **getDictionary(char filename[]) {
     return dictionary;
 }
 
-
+// Função para Obter as palavras de um arquivo
 char **getTextFromFile(char filename[], int *numberOfWords) {
     FILE *file;
     file = fopen(filename, "r");
 
+    // Arquivo não encontrado.
     if (file == NULL) {
         printf("ERRO! O arquivo %s não foi aberto!\n", filename);
         return NULL;
     }
 
+    // Array da linha atual, resultado do get e Array de palavras inicializados
     char line[281];
     char *result;
     char **text = NULL;
 
+    // Aloca o tamanho máximo que o array de palavras terá
     if (!(text = calloc(NWORDS_TEXT, sizeof *text))) {
         printf("Memória Virtual exaurida!\n");
         return NULL;
     }
 
+    // Inicializa o array de palavras da linha atual
+    // e o indicador da posição de cada palavra
     char **words = NULL;
     int i = 0;
+
+    // Enquanto o arquivo não acabar
     while (!feof(file)) {
+        // Obtém a linha atual
         result = fgets(line, 282, file);
         
+        // Inicializa a quantidade de palavras naquela linha como zero
         int nWordsOfLine = 0;
+        // Obtém o array de palavras naquela linha
         words = getWordsFromLine(result, &nWordsOfLine);
+        // Adiciona a quantidade de palavras naquela linha à quantidade total
         (*numberOfWords) += nWordsOfLine;
 
+        // Percorre todas as palavras da linha adicionando ao array de palavras final
         for (int j = 0; j < nWordsOfLine; j++) {
             text[i++] = words[j];
         }
@@ -189,36 +198,49 @@ char **getTextFromFile(char filename[], int *numberOfWords) {
     return text;
 }
 
+// Função para obter um array de palavras de uma linha
 char **getWordsFromLine(char line[], int *numberOfWords) {
+    // Inicializa o array de palavras
     char **words = NULL;
 
+    // Aloca o tamanho máximo do array de palavras
     if (!(words = calloc(NWORDS_TEXT, sizeof *words))) {
         printf("Memória Virtual exaurida!\n");
         return NULL;
     }
     
+    // Obtém o tamanho da linha
     size_t linelen = strlen(line);
 
+    // Retira o \n ao final da linha
     if (line[linelen - 1] == '\n') {
         line[linelen - 1] = 0;
     }
 
+    // Inicializa a string da palavra atual e os indicadores de posição
     int i = 0;
     int wordlen = 47;
     char word[wordlen];
     int currentChar = 0;
 
+    // Percorre cada char da linha
     for (int j = 0; j < linelen; j++) {
+        // Se o char é espaço, adiciona esse caractere à palavra atual
         if (line[j] == ' ') {
+            // adiciona a palavra completa ao array
             words[i++] = strdup(word);
+            // Aumenta a quantidade de palavras
             (*numberOfWords)++;
+            // Reseta a string
             memset(word, 0, wordlen);
             currentChar = 0;
         } else {
+            // Adiciona mais um caractere
             word[currentChar++] = line[j];
         }
     }
 
+    // Caso sobre uma palavra não adicionada
     if (strlen(word) > 0) {
         words[i++] = strdup(word);
         (*numberOfWords)++;
